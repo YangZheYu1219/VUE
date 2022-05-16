25253dc43c09ce61865074150d6e9771a9fd9cf0
YangZheYu1219 <ji3g420132@yahoo.com.tw>
2022-05-16 09:34:20 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<nav>
+  <ul>
+    <li v-for="id in 10">
+    <router-link :to="`/users/${id}`">{{ id }}使用者</router-link>
+    </li>
+  </ul>
+</nav>
+  <router-view/>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
